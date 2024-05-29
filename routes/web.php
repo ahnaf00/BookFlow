016a896e23017a55ee68c81718644c7fdd676194
kishan <jackson.kishan56@gmail.com>
2024-05-30 00:05:58 +0600

    Create category and Sub Category

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\SubCategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+Route::resource('category', CategoryController::class);
+
+Route::resource('sub-category', SubCategoryController::class);
